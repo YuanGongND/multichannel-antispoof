e1e9cf206b03a9b76b459cba5caafaa220aa3a76
ygong <yuangongfdu@gmail.com>
2020-06-30 16:46:54 -0400

    remove repeated files

--- multichannel-antispoofing/src/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2/15/20 11:12 PM
-# @Author  : Yuan Gong
-# @Affiliation  : University of Notre Dame
-# @Email   : yuangongfdu@gmail.com 
-# @File    : __init__.py
